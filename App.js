9ff3b3085e820497c396997fcf1ae96e40d0ea35
Ronel Gonzales <rjgonzales@fastgroup.biz>
2018-01-26 15:05:20 +0800

    add react-native-navigation

--- App.js
+++ App.js
@@ -1,91 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import { connect } from "react-redux";
 
-import React, { Component } from 'react';
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
+
+import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button
-} from 'react-native';
+  addPlace,
+  deletePlace,
+  selectPlace,
+  deselectPlace
+} from "./src/store/actions/index";
 
-import PlaceList from './src/components/PlaceList/PlaceList';
-import PlaceInput from './src/components/PlaceInput/PlaceInput';
-import PlaceImage from './src/assets/download.jpg';
-import PlaceDetail from './src/components/PlaceDetail/PlaceDetails'
-export default class App extends Component<{}> {
-
-  state = {
-    placeName: "",
-    places: [],
-    selectedPlace:null
-
+class App extends Component {
+  placeAddedHandler = placeName => {
+    this.props.onAddPlace(placeName);
   };
 
-  placeNameChangeHandler = val => {
-    this.setState({
-      placeName: val
-    });
+  placeDeletedHandler = () => {
+    this.props.onDeletePlace();
   };
 
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === "") {
-      return;
-    }
-
-    this.setState(prevState => {
-      return {
-        places: prevState.places.concat({
-          key: Math.random(),
-          name:prevState.placeName,
-          image: {
-            uri:'https://www.google.com.ph/imgres?imgurl=http%3A%2F%2Fi.ebayimg.com%2Fimages%2Fi%2F282513627716-0-1%2Fs-l1000.jpg&imgrefurl=https%3A%2F%2Fwww.ebay.com%2Fitm%2Fmp0031-Itachi-Uchiha-Eyes-Mangekyou-Sharingan-Round-Mouse-Pad-%2F282513627716&docid=VRTzIHdyPpTu0M&tbnid=CU5v0PyEabfyrM%3A&vet=10ahUKEwi5jeW-4e3YAhWLJZQKHYwtD5gQMwiqASgDMAM..i&w=640&h=480&hl=en&authuser=0&bih=647&biw=1366&q=sharingan&ved=0ahUKEwi5jeW-4e3YAhWLJZQKHYwtD5gQMwiqASgDMAM&iact=mrc&uact=8'
-          }
-        })
-      };
-    });
-    // this.setState({
-    //   placeName: ""
-    // });
-  };
-
-  placeDeletedHandler = key => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.filter(place => {
-          return  place.key !== key;
-        })
-      }
-    })
+  modalClosedHandler = () => {
+    this.props.onDeselectPlace();
   };
 
   placeSelectedHandler = key => {
-this.setState(prevState => {
-  return {
-    selectedPlace:prevState.places.find(place => {
-      return place.key === key;
-    })
-  }
-})
+    this.props.onSelectPlace(key);
   };
 
   render() {
-
     return (
       <View style={styles.container}>
-      <PlaceDetail selectedPlace={this.state.selectedPlace} />
-        <PlaceInput
-          placeName={this.state.placeName}
-          placeNameChangeHandler={this.placeNameChangeHandler}
-          placeSubmitHandler={this.placeSubmitHandler} />
+        <PlaceDetail
+          selectedPlace={this.props.selectedPlace}
+          onItemDeleted={this.placeDeletedHandler}
+          onModalClosed={this.modalClosedHandler}
+        />
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
         <PlaceList
-          places={this.state.places}
-          onItemSelected={this.placeSelectedHandler} />
+          places={this.props.places}
+          onItemSelected={this.placeSelectedHandler}
+        />
       </View>
     );
   }
@@ -94,10 +51,27 @@ this.setState(prevState => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-
+    padding: 26,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  }
 });
+
+const mapStateToProps = state => {
+  return {
+    places: state.places.places,
+    selectedPlace: state.places.selectedPlace
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPlace: name => dispatch(addPlace(name)),
+    onDeletePlace: () => dispatch(deletePlace()),
+    onSelectPlace: key => dispatch(selectPlace(key)),
+    onDeselectPlace: () => dispatch(deselectPlace())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/screens/Auth/Auth.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View } from 'react-native';
+
+class AuthScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Login Screen
+                </Text>
+            </View>
+        )
+    };
+}
